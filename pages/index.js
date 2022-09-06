20e219d79fc238fe37b48d2749885d2aedc13e92
Georgi Karadzhov <jorkufrbg@gmail.com>
2022-09-06 03:12:42 +0300

    Starting Masonry Grid

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const HomePage = (props) => {
   return (
     <Fragment>
       <Head>
-        <title>React Meetups</title>
+        <title>React Spots</title>
         <meta
           name="description"
           content="Browse a huge list of highly active React meetups!"

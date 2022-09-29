d92c3dfc3f9f6fceb1ea76f8051762998e8a763b
Georgi Karadzhov <jorkufrbg@gmail.com>
2022-09-29 04:44:31 +0300

    removed unnecessary ;

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -31,7 +31,7 @@ const NewMeetupPage = () => {
           content="Add your own meetups and create amazing networking opportunities"
         />
       </Head>
-      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </Fragment>
   );
 };
